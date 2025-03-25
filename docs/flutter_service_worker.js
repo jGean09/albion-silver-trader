3c101c36bea26ba8ce16c9f837c0a73f324a25e6
jGean09 <josegeantlc@gmail.com>
2025-03-24 21:22:19 -0300

    Build web atualizado

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "4ded0e1d5428df6bb424668353ab9d5b",
+"flutter_bootstrap.js": "73a93855a2f873ad59bd0deb7b34cc2a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
